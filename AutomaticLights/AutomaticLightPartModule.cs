2d0fab89d41a81c500e6cdd34fa9a1facfc84234
Davy J <dave_jones147@hotmail.com>
2016-07-27 12:25:40 +0200

    First commit- before I begin testing

---
+++ AutomaticLights/AutomaticLightPartModule.cs
@@ -0,0 +1,117 @@
+﻿namespace AutomaticLights
+{
+    using System.Linq;
+
+    public class AutomaticLightPartModule : PartModule
+    {
+        [KSPField()]
+        public bool checkResource;
+
+        /// <summary>
+        /// This value should be a percentage 0.0 -&gt; 1.0
+        /// </summary>
+        [KSPField()]
+        public double minResourceLevel;
+
+        [KSPField()]
+        public string resourceName;
+
+        private const string ElectricCharge = "ElectricCharge";
+        private static int counter = 0;
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            if (counter++ % 100 != 0)
+            {
+                // only update every 100 frames.
+                return;
+            }
+
+            counter = 1;
+
+            var vessel = FlightGlobals.ActiveVessel;
+            if (vessel.state != Vessel.State.ACTIVE)
+            {
+                return;
+            }
+
+            if (vessel.situation != Vessel.Situations.PRELAUNCH)
+            {
+                // don't drain power on the pad.
+                return;
+            }
+
+            if (checkResource)
+            {
+                var activeResources = vessel.GetActiveResources();
+                Vessel.ActiveResource rex = null;
+                foreach (var r in activeResources)
+                {
+                    if (r.info.name == resourceName.ValueOrDefault(ElectricCharge))
+                    {
+                        rex = r;
+                        break;
+                    }
+                }
+
+                if (rex == null)
+                {
+                    UnityEngine.Debug.Log("Missing resource " + resourceName.ValueOrDefault(ElectricCharge));
+                    return;
+                }
+
+                // Check that we have the required resource.
+                if (rex.amount > 0 && rex.amount / rex.maxAmount > minResourceLevel)
+                {
+                    TurnOnOffAuto();
+                }
+                else
+                {
+                    TurnOnOff(false);
+                }
+            }
+        }
+
+        private void TurnOnOff(bool turnOn)
+        {
+            var vessel = FlightGlobals.ActiveVessel;
+            if (vessel != null)
+            {
+                foreach (var p in vessel.GetActiveParts())
+                {
+                    foreach (var m in p.Modules)
+                    {
+                        var ml = m as ModuleLight;
+                        if (ml != null)
+                        {
+                            ml.SetLightState(turnOn);
+                            break; // this part shouldn't have more than one module light.
+                        }
+                    }
+                }
+            }
+        }
+
+        private void TurnOnOffAuto()
+        {
+            var vessel = FlightGlobals.ActiveVessel;
+            if (vessel != null)
+            {
+                var sun = FlightGlobals.Bodies.FirstOrDefault();
+                if (sun != null)
+                {
+                    // if the sun is null, we've got bigger problems
+                    if (Utilities.RaytraceBody(vessel, sun))
+                    {
+                        TurnOnOff(false);
+                    }
+                    else
+                    {
+                        TurnOnOff(true);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AutomaticLights/Utilities.cs
@@ -0,0 +1,50 @@
+﻿namespace AutomaticLights
+{
+    public static class Utilities
+    {
+        /// <summary> return true if the ray 'dir' starting at 'p' doesn't hit 'body' </summary>
+        /// <param name="p">ray Origin</param> <param name="dir">ray direction</param> <param
+        /// name="body">obstacle</param> <returns></returns> <remarks> Author : ShotgunNinja - http://forum.kerbalspaceprogram.com/index.php?/topic/144484-auto-lights-at-night/&do=findComment&comment=2691985</remarks>
+        public static bool Raytrace(Vector3d p, Vector3d dir, CelestialBody body)
+        {
+            // ray from origin to body center
+            var diff = body.position - p;
+
+            // projection of origin->body center ray over the raytracing direction
+            double k = Vector3d.Dot(diff, dir);
+
+            // the ray doesn't hit body if its minimal analytical distance along the ray is less than
+            // its radius
+            return k < 0.0 || (dir * k - diff).magnitude > body.Radius;
+        }
+
+        /// <summary> return true if the body is visible from the vessel </summary> <param
+        /// name="vessel">vessel: origin</param> <param name="body">body: target</param>
+        /// <returns>return: true if visible, false otherwise</returns> <remarks> Author :
+        /// ShotgunNinja - http://forum.kerbalspaceprogram.com/index.php?/topic/144484-auto-lights-at-night/&do=findComment&comment=2691985</remarks>
+        public static bool RaytraceBody(Vessel vessel, CelestialBody body)
+        {
+            // shortcuts
+            CelestialBody mainbody = vessel.mainBody;
+            CelestialBody refbody = mainbody.referenceBody;
+
+            // generate ray parameters
+            var vessel_pos = vessel.GetWorldPos3D();
+            var dir = (body.position - vessel_pos).normalized;
+
+            // raytrace
+            return (body == mainbody || Raytrace(vessel_pos, dir, mainbody))
+                && (body == refbody || refbody == null || Raytrace(vessel_pos, dir, refbody));
+        }
+
+        public static string ValueOrDefault(this string value, string defaultValue)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return defaultValue;
+            }
+
+            return value;
+        }
+    }
+}
